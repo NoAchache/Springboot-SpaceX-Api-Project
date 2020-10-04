5147ce1c7ecc8e9c924bbb220889c9a7272dddf7
noe <noe.achache@gmail.com>
2020-10-04 12:45:29 +0200

    initialize project

---
+++ src/test/java/com/example/SimpleSpringbootProject/SimpleSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleSpringbootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
